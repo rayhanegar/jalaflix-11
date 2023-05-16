0b3a6b0e957ed2342279e1693d490a3f826314d6
zenonsenn <zenonsenn@gmail.com>
2023-05-16 21:12:05 +0700

    more features
    bisa logout, bisa akses page history dan upgrade tier

--- src/UserApp.java
+++ src/UserApp.java
@@ -5,7 +5,70 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+class Upgrade {
+    public static JFrame upgradePage = new JFrame("Upgrade tier");
+
+    Upgrade() {
+        JLabel test = new JLabel("Halo dari upgrade page");
+        JButton back = new JButton("go back");
+
+        FlowLayout fl = new FlowLayout();
+
+        upgradePage.setSize(800, 600);
+        upgradePage.setLayout(fl);
+        upgradePage.setVisible(false);
+        upgradePage.setResizable(true);
+        upgradePage.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+
+        upgradePage.add(test);
+        upgradePage.add(back);
+
+        back.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                UserApp.mainApp.setVisible(true);
+                upgradePage.setVisible(false);
+                UserApp.mainApp.repaint();
+                UserApp.mainApp.revalidate();
+            }
+        });
+    }
+}
+
+class History {
+    public static JFrame HistoryPage = new JFrame("History page");
+
+    History() {
+        JLabel test = new JLabel("Halo dari history page");
+        JButton back = new JButton("go back");
+
+        FlowLayout fl = new FlowLayout();
+
+        HistoryPage.setSize(800, 600);
+        HistoryPage.setLayout(fl);
+        HistoryPage.setVisible(false);
+        HistoryPage.setResizable(true);
+        HistoryPage.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+
+        HistoryPage.add(test);
+        HistoryPage.add(back);
+
+        back.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                UserApp.mainApp.setVisible(true);
+                HistoryPage.setVisible(false);
+                UserApp.mainApp.repaint();
+                UserApp.mainApp.revalidate();
+            }
+        });
+    }
+}
+
 public class UserApp {
+    public static JFrame mainApp = new JFrame("Jalaflix-11");
+    public static JFrame logUser = new JFrame("Login/Register");
+
     public static void main(String[] args) {
         // var
         Database db = new Database();
@@ -16,15 +79,29 @@ public class UserApp {
 
         // content and layout
         GridLayout gl = new GridLayout(0, 4, 16, 16);
-        GridLayout gl2 = new GridLayout(2, 1);
-        JFrame jf = new JFrame("Jalaflix-11");
-        JFrame logUser = new JFrame("Login/Register");
+        // GridLayout gl2 = new GridLayout(2, 1);
+
         JLabel logUserL1 = new JLabel("di sini nanti ada fitur login. login = bikin akun baru biar gampang");
         JButton login = new JButton("login (button sementara)");
-        JButton logout = new JButton("Log out");
-        JPanel jp1 = new JPanel(gl);
-        JPanel navbar = new JPanel();
-        JScrollPane jsp = new JScrollPane(jp1,
+
+        JMenu account = new JMenu("Account");
+        JMenuItem upgrade = new JMenuItem("Upgrade");
+        JMenuItem history = new JMenuItem("History");
+        JMenuItem logout = new JMenuItem("Logout");
+        account.setFont(new Font("Arial", Font.PLAIN, 18));
+        upgrade.setFont(new Font("Arial", Font.PLAIN, 18));
+        history.setFont(new Font("Arial", Font.PLAIN, 18));
+        logout.setFont(new Font("Arial", Font.PLAIN, 18));
+        account.add(upgrade);
+        account.add(history);
+        account.add(logout);
+        JMenuBar navbar = new JMenuBar();
+        navbar.add(account);
+
+        // JButton logout = new JButton("Log out");
+        JPanel movieHandler = new JPanel(gl);
+        // JPanel navbar = new JPanel();
+        JScrollPane jsp = new JScrollPane(movieHandler,
                 ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS,
                 ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED);
         jsp.getVerticalScrollBar().setUnitIncrement(12);
@@ -32,16 +109,18 @@ public class UserApp {
 
         // border
         Border b = BorderFactory.createEmptyBorder(36, 36, 36, 36);
-        Border b2 = BorderFactory.createEmptyBorder(36, 6, 36, 6);
+        Border b2 = BorderFactory.createEmptyBorder(0, 0, 36, 0);
+        Border b3 = BorderFactory.createEmptyBorder(0, 12, 0, 0);
 
         // prelim set window
-        jf.setSize(800, 600); // main program window
+        mainApp.setSize(800, 600); // main program window
         logUser.setSize(800, 600);
         logUserL1.setSize(logUser.getWidth(), logUser.getHeight());
         login.setBounds(36, 36, 360, 36);
-        jp1.setSize(800, 600); // isinya film
-        logout.setSize(jf.getWidth(), 100);
+        movieHandler.setSize(800, 600); // isinya film
+        // logout.setSize(mainApp.getWidth(), 100);
 
+        // login doang
         logUser.setLayout(null);
         logUser.setVisible(true);
         logUser.setResizable(true);
@@ -51,27 +130,64 @@ public class UserApp {
         logUser.add(login);
         logUser.add(logUserL1);
 
-        jf.setLayout(gl2);
-        jf.setVisible(false);
-        jf.setResizable(true);
-        jf.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        navbar.add(logout);
-        jp1.setBorder(b);
+        // aplikasi utama
+        mainApp.setVisible(false);
+        mainApp.setResizable(true);
+        mainApp.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        // navbar.add(logout);
+        movieHandler.setBorder(b);
+
+        // urusan dengan yth. navbar
+        navbar.setBorder(b3);
+        mainApp.setJMenuBar(navbar);
 
         // logic
         login.addActionListener(new ActionListener() {
+            @Override
             public void actionPerformed(ActionEvent ae) {
-                jf.setVisible(true);
+                mainApp.setVisible(true);
                 logUser.setVisible(false);
-                jf.repaint();
+                mainApp.repaint();
+                mainApp.revalidate();
             }
         });
 
         logout.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent ae) {
-                logUser.setVisible(true);
-                jf.setVisible(false);
-                logUser.repaint();
+            @Override
+            public void actionPerformed(ActionEvent ngikngok) {
+                int result = JOptionPane.showConfirmDialog(mainApp, "apakah anda adus riil?", "riil adus kah?",
+                        JOptionPane.YES_NO_OPTION);
+                logout.setSelected(true);
+                if (result == JOptionPane.YES_OPTION) {
+                    logUser.setVisible(true);
+                    mainApp.setVisible(false);
+                    logUser.repaint();
+                    logUser.revalidate();
+                } else if (result == JOptionPane.NO_OPTION) {
+                    logout.setSelected(false);
+                }
+            }
+        });
+
+        upgrade.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                new Upgrade(); // instance it first
+                Upgrade.upgradePage.setVisible(true);
+                mainApp.setVisible(false);
+                Upgrade.upgradePage.repaint();
+                Upgrade.upgradePage.revalidate();
+            }
+        });
+
+        history.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                new History(); // instance it first
+                History.HistoryPage.setVisible(true);
+                mainApp.setVisible(false);
+                History.HistoryPage.repaint();
+                History.HistoryPage.revalidate();
             }
         });
 
@@ -87,12 +203,13 @@ public class UserApp {
             movies[i].setPreferredSize(new Dimension(200, 350));
             movies[i].setBackground(white);
             movies[i].add(infoText[i]);
-            jp1.add(movies[i]);
+            movieHandler.add(movies[i]);
         }
-        jf.add(navbar);
-        jf.add(jsp);
+        // mainApp.add(navbar);
+        mainApp.add(jsp);
 
         logUser.repaint();
-        jf.repaint();
+        mainApp.repaint();
+        mainApp.revalidate();
     }
 }
